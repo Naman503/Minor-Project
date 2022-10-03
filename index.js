1f339a6b3bbbe60d6dd7b581980d27d9cbe6a816
Naman <namanpathak503@gmail.com>
2022-10-03 19:34:39 +0530

    basic branch in link

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ async function start() {
       labels.map(async label => {
         const descriptions = []
         for (let i = 1; i <= 2; i++) {
-          const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/Naman503/Minor-Project/main/labeled_images/${label}/${i}.jpg`)
+          const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/Naman503/Minor-Project/basic/labeled_images/${label}/${i}.jpg`)
           const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
           descriptions.push(detections.descriptor)
         }
