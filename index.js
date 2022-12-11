244053ed6f795c52c59fa2fc8fcc5c2be17883f5
Naman <namanpathak503@gmail.com>
2022-12-11 20:33:44 +0530

    surver and index improve

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
   faceapi.nets.faceExpressionNet.loadFromUri("./models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("./models")
 ]).then(enableAllButton());
 
 function enableAllButton() {
