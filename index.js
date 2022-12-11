422d34dccc8aba89cfd5917c55bfe061aa9f5720
Naman <namanpathak503@gmail.com>
2022-12-11 20:56:40 +0530

    ssd module

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const TakePhotoBtn = document.getElementById("TakePhoto");
 const imageUploadActionFrame = document.getElementById("imageUpload");
 
 Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
   faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
   faceapi.nets.faceExpressionNet.loadFromUri("./models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("./models")
 ]).then(enableAllButton());
 
 function enableAllButton() {
